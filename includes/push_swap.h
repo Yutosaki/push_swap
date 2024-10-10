6952f211882b388ef869f06f19e84b066e0d224c
Yutosaki <sasayu8190@gmail.com>
2024-10-10 22:09:52 +0900

    feat: add header file

---
+++ includes/push_swap.h
@@ -0,0 +1,30 @@
+#ifndef PUSH_SWAP_H
+#define PUSH_SWAP_H
+// #include "../libft.h"
+
+
+typedef struct s_node
+{
+    int             value;
+    struct s_node   *next;
+} t_node;
+
+typedef struct s_stack
+{
+    t_node  *top;
+    int     size;
+} t_stack;
+
+void    sa(t_stack *a);
+void    sb(t_stack *b);
+void    ss(t_stack *a, t_stack *b);
+void    pa(t_stack *a, t_stack *b);
+void    pb(t_stack *a, t_stack *b);
+void    ra(t_stack *a);
+void    rb(t_stack *b);
+void    rr(t_stack *a, t_stack *b);
+void    rra(t_stack *a);
+void    rrb(t_stack *b);
+void    rrr(t_stack *a, t_stack *b);
+
+#endif // PUSH_SWAP_H
