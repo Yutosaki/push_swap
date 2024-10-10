7e4db7b026fbc7de7451c0e3030f141dae5923f6
Yutosaki <sasayu8190@gmail.com>
2024-10-10 21:06:19 +0900

    feat: formatter

--- src/instractions/swap.c
+++ src/instractions/swap.c
@@ -1,31 +1,31 @@
 #include "push_swap.h"
 
-void sa(t_stack *a)
+void	sa(t_stack *a)
 {
-    t_node  *tmp;
+	t_node	*tmp;
 
-    if (a->size < 2)
-        return ;
-    tmp = a->top;
-    a->top = a->top->next;
-    tmp->next = a->top->next;
-    a->top->next = tmp;
+	if (a->size < 2)
+		return ;
+	tmp = a->top;
+	a->top = a->top->next;
+	tmp->next = a->top->next;
+	a->top->next = tmp;
 }
 
-void sb(t_stack *b)
+void	sb(t_stack *b)
 {
-    t_node  *tmp;
+	t_node	*tmp;
 
-    if (b->size < 2)
-        return ;
-    tmp = b->top;
-    b->top = b->top->next;
-    tmp->next = b->top->next;
-    b->top->next = tmp;
+	if (b->size < 2)
+		return ;
+	tmp = b->top;
+	b->top = b->top->next;
+	tmp->next = b->top->next;
+	b->top->next = tmp;
 }
 
-void ss(t_stack *a, t_stack *b)
+void	ss(t_stack *a, t_stack *b)
 {
-    sa(a);
-    sb(b);
+	sa(a);
+	sb(b);
 }
