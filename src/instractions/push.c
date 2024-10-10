6ccb824176c0837f1686b3ef78aff8ee609aa202
Yutosaki <sasayu8190@gmail.com>
2024-10-10 20:02:20 +0900

    feat: add push func

---
+++ src/instractions/push.c
@@ -0,0 +1,29 @@
+#include "push_swap.h"
+
+void pa(t_stack *a, t_stack *b)
+{
+    t_node  *tmp;
+
+    if (b->size == 0)
+        return ;
+    tmp = b->top;
+    b->top = b->top->next;
+    tmp->next = a->top;
+    a->top = tmp;
+    a->size++;
+    b->size--;
+}
+
+void pb(t_stack *a, t_stack *b)
+{
+    t_node  *tmp;
+
+    if (a->size == 0)
+        return ;
+    tmp = a->top;
+    a->top = a->top->next;
+    tmp->next = b->top;
+    b->top = tmp;
+    a->size--;
+    b->size++;
+}
